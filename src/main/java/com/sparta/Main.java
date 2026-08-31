51d42f5652461d4a9b754e93a1466c6badaf099d
leejunhyeok3907 <leejh3907@naver.com>
2026-08-31 19:54:47 +0900

    feat: print student score

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -5,5 +5,9 @@ package com.sparta;
 public class Main {
     public static void main(String[] args) {
 
+        String name = "김하나";
+        int score = 87;
+
+        System.out.println(name + " 학생의 점수는 " + score + "점입니다.");
     }
 }
